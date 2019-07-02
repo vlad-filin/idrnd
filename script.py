c428d431b1900e9a3667f9d91259eb4a9eb90144
Vladislav Filimonov <ifilin45@gmail.com>
2019-07-02 09:27:03 +0300

    modify script&baseline

--- script.py
+++ script.py
@@ -3,16 +3,29 @@ import pandas as pd
 import torch
 import tqdm
 from utils import read_scipy
-from models import ModelWrapper_TorchVisionNet
+from models import TorchVisionNet_with_exctractor
+import DftSpectrogram_pytorch
 from MobileNetV2_torchvision import  MobileNetV2
 import torch.nn as nn
 from librosa.feature import mfcc
 
 print("Done!")
+
 dataset_dir = "."
+dft_conf = {"length": 512,
+            "shift": 256,
+            "nfft": 512,
+            "mode": 'log',
+            "normalize_feature": True}
+
+
+dft_pytorch = DftSpectrogram_pytorch.DftSpectrogram(**dft_conf)
 MN2 = MobileNetV2()
 MN2.classifier[1] = nn.Linear(1280, 2, bias=False)
-model = ModelWrapper_TorchVisionNet(MN2).to('cuda')
+MN2.features[0][0] = nn.Conv2d(1, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
+model = TorchVisionNet_with_exctractor(MN2, dft_pytorch).to('cuda')
+
+
 model.load_state_dict((torch.load("weights.pt")))
 model.eval()
 print("model Done!")
@@ -26,9 +39,9 @@ print(eval_protocol.sample(5).head())
 
 for protocol_id, protocol_row in tqdm.tqdm(list(eval_protocol.iterrows())):
     feature = read_scipy(os.path.join(dataset_dir, protocol_row['path']))
-    feature = mfcc(feature)[None, None,...]
-    feature = torch.tensor(feature, dtype=torch.float32).to('cuda')
-    score = model.softmax(model(feature)).cpu()
+    with torch.no_grad():
+        feature = torch.tensor(feature, dtype=torch.float32).to('cuda')
+        score = model.softmax(model(feature)).cpu()
     eval_protocol.at[protocol_id, 'score'] = score[0][0]
 eval_protocol[['path', 'score']].to_csv('answers.csv', index=None)
 print(eval_protocol.sample(5).head())
