b0bccde1869ccd0228b094e4de52fc9f9e57f6e1
v.filimonov <v.filimonov@partner.samsung.com>
2019-07-10 17:24:53 +0300

    add Non Trainable extractor

--- models.py
+++ models.py
@@ -77,3 +77,25 @@ class TwoBranchModel(nn.Module):
         res = self.linear(v)
         # x = self.softmax(x)
         return res
+
+
+class TwoBranchModelNTE(nn.Module):
+
+    def __init__(self, NetMfcc, NetDft, extractorNT, num_features=1024):
+        super(TwoBranchModelNTE, self).__init__()
+
+        self.NetMfcc = NetMfcc
+        self.NetDft = NetDft
+        self.extractorNT = extractorNT
+        self.linear = nn.Linear(num_features, 2, bias=False)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x, mfcc):
+
+        x0 = self.extractorNT(x)
+        v0 = self.NetDft(x0)
+        v1 = self.NetMfcc(mfcc)
+        v =  torch.cat((v0, v1), dim=1)
+        res = self.linear(v)
+        # x = self.softmax(x)
+        return res
