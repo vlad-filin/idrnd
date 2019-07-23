b5bebb370d01391d214363bc01ef96b50ce0410f
v.filimonov <v.filimonov@partner.samsung.com>
2019-07-23 17:32:13 +0300

    mod scr

--- train_scripts/train2_NTE_add_data.py
+++ train_scripts/train2_NTE_add_data.py
@@ -1,5 +1,5 @@
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+#os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 import sys
 sys.path.insert(0, "../")
 import numpy as np
@@ -7,7 +7,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
-from densenet import densenet121
+from densenet import densenet169
 
 from models import TwoBranchModelNTE
 from dataset import VoiceAntiSpoofDataset
@@ -21,6 +21,8 @@ from kekas import Keker, DataOwner
 from kekas.metrics import accuracy
 from utils import step_fn, ScoreCallback, exp_decay
 import torch.autograd as autograd
+from librosa.feature import melspectrogram
+
 
 dft_conf0 = {"length": 512,
             "shift": 256,
@@ -35,18 +37,19 @@ DN2_Dft = densenet169()
 DN2_Dft.features[0] = nn.Conv2d(1, 64, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
 DN2_MFCC = densenet169()
 DN2_MFCC.features[0] = nn.Conv2d(1, 64, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
-model = TwoBranchModelNTE(DN2_MFCC, DN2_Dft, dft_pytorchNT, num_features=2048).to('cuda')
+model = TwoBranchModelNTE(DN2_MFCC, DN2_Dft, dft_pytorchNT, num_features=3328).to('cuda')
 
 
 
-
+#mfcc_function = lambda data: mfcc(data, sr=16000, n_mfcc=45)
+mel_function = lambda data: melspectrogram(data, sr=16000, n_mels=128)
 dataset_dir = '../../Training_Data/'
 add_data_dir = '../../validationASV/'
 print("Num samples:", len(glob.glob(os.path.join(dataset_dir, '**/*.wav'), recursive=True)))
 dataset = VoiceAntiSpoofDataset(dataset_dir, 'all', read_scipy,
                                transform=[lambda x: x[None, ...].astype(np.float32)],
-
-                                add_TData=add_data_dir)
+                                add_TData=add_data_dir,
+                                mfcc_function=mel_function)
 
 
 """
@@ -58,7 +61,7 @@ dataset_val = VoiceAntiSpoofDataset(dataset_val_dir, 'all', read_scipy,
                                    transform=[lambda x: x[None, ...].astype(np.float32)])
 
 sampler = torch.utils.data.sampler.WeightedRandomSampler(dataset.weights, len(dataset.weights))
-batch_size = 256
+batch_size = 192
 num_workers = 16
 
 #dataset.data = dataset.data[0:24] + dataset.data[-24:]
@@ -83,8 +86,8 @@ keker = Keker(model=model,
                                                   # an SGD is using by default
               opt_params={"weight_decay": 1e-3},
               callbacks=[ScoreCallback('preds', 'label', compute_err,
-                                       'checkpoints/2branch_NTE_addData45',
-                                       logdir='tensorboard/2branch_NTE_addData45')],
+                                       'checkpoints/2branch_mel128_addData_DN',
+                                       logdir='tensorboard/2branch_mel128_addData_DN')],
                  metrics={"acc": accuracy})
 with autograd.detect_anomaly():
     keker.kek(lr=1e-3,
@@ -92,10 +95,10 @@ with autograd.detect_anomaly():
               sched=torch.optim.lr_scheduler.MultiStepLR,       # pytorch lr scheduler class
               sched_params={"milestones": [15, 25, 35, 45], "gamma": 0.5},
              cp_saver_params={
-                  "savedir": "checkpoints/2branch_NTE_addData45",
+                  "savedir": "checkpoints/2branch_mel128_addData_DN",
              "metric":"acc",
              "mode":'max'},
-              logdir="tensorboard/2branch_NTE_addData45")
+              logdir="tensorboard/2branch_mel128_addData_DN")
 
 
-torch.save(model.state_dict(), "checkpoints/2branch_NTE_addData45/final.pt")
+torch.save(model.state_dict(), "checkpoints/2branch_mel128_addData_DN/final.pt")
